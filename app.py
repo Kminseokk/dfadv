1d44bb6682ff15599230c40415cfe3e8d8b6e8e6
MinSeokk <74297468+Kminseokk@users.noreply.github.com>
2024-10-12 16:50:07 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -175,5 +175,8 @@ def dun_update():
     return dun_update_result
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+# if __name__ == '__main__':
+#     app.run(debug=True)
